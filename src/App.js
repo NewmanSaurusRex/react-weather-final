03687074891e5d5caacd2a3486ce75655482c0b7
MNewman <megan.m.boggs@gmail.com>
2022-06-20 23:34:46 -0700

    added github repo link, h1, and footer

--- src/App.js
+++ src/App.js
@@ -2,5 +2,25 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">React Weather Application - Final</div>;
+  return (
+    <div className="App">
+      <h1>React Weather Application - Final</h1>
+      <footer>
+        {" "}
+        This project was coded by{" "}
+        <a href="https://mnewman.io" target="_blank" rel="noreferrer">
+          MNewman
+        </a>{" "}
+        and is {""}
+        <a
+          href="https://github.com/NewmanSaurusRex/react-weather-final"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced
+        </a>
+        {""} on github.
+      </footer>
+    </div>
+  );
 }
