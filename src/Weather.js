016e73920c9898c64f735f2627c0a00ba80adf78
MNewman <megan.m.boggs@gmail.com>
2022-06-25 01:30:13 -0700

    fixed bug with weather icon

--- src/Weather.js
+++ src/Weather.js
@@ -12,7 +12,7 @@ export default function Weather(props) {
       temperature: response.data.main.temp,
       wind: response.data.wind.speed,
       description: response.data.weather[0].description,
-      iconUrl: response.data.weather[0].icon,
+      icon: response.data.weather[0].icon,
       humidity: response.data.main.humidity,
       city: response.data.name,
       date: new Date(response.data.dt * 1000),
