6b92ac8cf538013641021ed155c828865aa3dbee
MNewman <megan.m.boggs@gmail.com>
2022-06-25 23:17:05 -0700

    generated new api call to gather lat/lon of location to send to weather forecast
    This new api will give access to the forecasted temps, while the previous only gave access to current info 💪

--- src/Weather.js
+++ src/Weather.js
@@ -10,6 +10,7 @@ export default function Weather(props) {
   function handleResponse(response) {
     setWeatherData({
       ready: true,
+      coordinates: response.data.coord,
       temperature: response.data.main.temp,
       wind: response.data.wind.speed,
       description: response.data.weather[0].description,
@@ -59,7 +60,7 @@ export default function Weather(props) {
           </div>
         </form>
         <WeatherInfo data={weatherData} />
-        <WeatherForecast />
+        <WeatherForecast coordinates={weather.coordinates}/>
       </div>
     );
   } else {
